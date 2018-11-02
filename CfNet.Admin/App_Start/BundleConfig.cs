7be608e6545986b4d56cda63876aad3852964857
hughlau <hughlaunet@gmail.com>
2018-11-02 16:51:39 +0800

    vue commit

--- CfNet.Admin/App_Start/BundleConfig.cs
+++ CfNet.Admin/App_Start/BundleConfig.cs
@@ -25,6 +25,9 @@ namespace CfNet.Admin
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/vue").Include(
+                "~/Scripts/Vue.min.js"));
         }
     }
 }
